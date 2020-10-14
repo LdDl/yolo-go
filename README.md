49fdc2965e3e9ce1a9154463a064bea89d050fb9
Dimitrii Lopanov <sexykdi@gmail.com>
2020-10-14 09:29:42 +0300

    split detecter/training calls

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # WIP
 Port of [Darknet YOLO](https://github.com/pjreddie/darknet#darknet), but via [Gorgonia](https://github.com/gorgonia/gorgonia)
+
+
+
+# Usage
+Navigate to [examples/tiny-v3](examples/tiny-v3) folder and run [main.go](examples/tiny-v3/main.go).
+
+For testing:
+```shell
+go run main.go --mode detector --cfg ../../test_network_data/yolov3-tiny.cfg --weights ../../test_network_data/yolov3-tiny.weights --image ../../test_network_data/dog_416x416.jpg
+```
--- example/tiny-v3/main.go
+++ example/tiny-v3/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 	"time"
 
 	yologo "github.com/LdDl/yolo-go"
@@ -10,20 +12,27 @@ import (
 )
 
 var (
-	imgWidth       = 416
-	imgHeight      = 416
-	channels       = 3
-	boxes          = 3
-	leakyCoef      = 0.1
-	weights        = "../../test_network_data/yolov3-tiny.weights"
-	cfg            = "../../test_network_data/yolov3-tiny.cfg"
-	imagePath      = "../../test_network_data/dog_416x416.jpg"
+	imgWidth  = 416
+	imgHeight = 416
+	channels  = 3
+	boxes     = 3
+	leakyCoef = 0.1
+
+	modeStr        = flag.String("mode", "detector", "Choose the mode: detector/training")
+	weights        = flag.String("weights", "../../test_network_data/yolov3-tiny.weights", "Path to weights file")
+	cfg            = flag.String("cfg", "../../test_network_data/yolov3-tiny.cfg", "Path to net configuration file")
+	imagePath      = flag.String("image", "../../test_network_data/dog_416x416.jpg", "Path to image file for 'detector' mode")
+	trainingFolder = flag.String("train", "./data/test_yolo_op", "Path to folder with labeled data")
+
 	cocoClasses    = []string{"person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"}
 	scoreThreshold = float32(0.8)
 	iouThreshold   = float32(0.3)
 )
 
 func main() {
+	// Parse flags
+	flag.Parse()
+
 	// Create new graph
 	g := gorgonia.NewGraph()
 
@@ -31,57 +40,67 @@ func main() {
 	input := gorgonia.NewTensor(g, tensor.Float32, 4, gorgonia.WithShape(1, channels, imgWidth, imgHeight), gorgonia.WithName("input"))
 
 	// Prepare YOLOv3 tiny vartiation
-	model, err := yologo.NewYoloV3Tiny(g, input, len(cocoClasses), boxes, leakyCoef, cfg, weights)
+	model, err := yologo.NewYoloV3Tiny(g, input, len(cocoClasses), boxes, leakyCoef, *cfg, *weights)
 	if err != nil {
 		fmt.Printf("Can't prepare tiny-YOLOv3 network due the error: %s\n", err.Error())
 		return
 	}
 	model.Print()
 
-	// Parse image file as []float32
-	imgf32, err := yologo.GetFloat32Image(imagePath, imgHeight, imgWidth)
-	if err != nil {
-		fmt.Printf("Can't read []float32 from image due the error: %s\n", err.Error())
+	switch strings.ToLower(*modeStr) {
+	case "detector":
+		// Parse image file as []float32
+		imgf32, err := yologo.GetFloat32Image(*imagePath, imgHeight, imgWidth)
+		if err != nil {
+			fmt.Printf("Can't read []float32 from image due the error: %s\n", err.Error())
+			return
+		}
+
+		// Prepare image tensor
+		image := tensor.New(tensor.WithShape(1, channels, imgHeight, imgWidth), tensor.Of(tensor.Float32), tensor.WithBacking(imgf32))
+
+		// Fill input tensor with data from image tensor
+		err = gorgonia.Let(input, image)
+		if err != nil {
+			fmt.Printf("Can't let input = []float32 due the error: %s\n", err.Error())
+			return
+		}
+
+		// Prepare new Tape machine
+		tm := gorgonia.NewTapeMachine(g)
+		defer tm.Close()
+
+		// Do forward path through the neural network (YOLO)
+		st := time.Now()
+		if err := tm.RunAll(); err != nil {
+			fmt.Printf("Can't run tape machine due the error: %s\n", err.Error())
+			return
+		}
+		fmt.Println("Feedforwarded in:", time.Since(st))
+
+		// Do not forget to reset Tape machine (usefully when doing RunAll() in a loop)
+		tm.Reset()
+
+		// Postprocessing YOLO's output
+		st = time.Now()
+		dets, err := model.ProcessOutput(cocoClasses, scoreThreshold, iouThreshold)
+		if err != nil {
+			fmt.Printf("Can't do postprocessing due error: %s", err.Error())
+			return
+		}
+		fmt.Println("Postprocessed in:", time.Since(st))
+
+		fmt.Println("Detections:")
+		for i := range dets {
+			fmt.Println(dets[i])
+		}
+
+		break
+	case "training":
+		break
+	default:
+		fmt.Printf("Mode '%s' is not implemented", *modeStr)
 		return
-	}
-
-	// Prepare image tensor
-	image := tensor.New(tensor.WithShape(1, channels, imgHeight, imgWidth), tensor.Of(tensor.Float32), tensor.WithBacking(imgf32))
-
-	// Fill input tensor with data from image tensor
-	err = gorgonia.Let(input, image)
-	if err != nil {
-		fmt.Printf("Can't let input = []float32 due the error: %s\n", err.Error())
-		return
-	}
-
-	// Prepare new Tape machine
-	tm := gorgonia.NewTapeMachine(g)
-	defer tm.Close()
-
-	// Do forward path through the neural network (YOLO)
-	st := time.Now()
-	if err := tm.RunAll(); err != nil {
-		fmt.Printf("Can't run tape machine due the error: %s\n", err.Error())
-		return
-	}
-	fmt.Println("Feedforwarded in:", time.Since(st))
-
-	// Do not forget to reset Tape machine (usefully when doing RunAll() in a loop)
-	tm.Reset()
-
-	// Postprocessing YOLO's output
-	st = time.Now()
-	dets, err := model.ProcessOutput(cocoClasses, scoreThreshold, iouThreshold)
-	if err != nil {
-		fmt.Printf("Can't do postprocessing due error: %s", err.Error())
-		return
-	}
-	fmt.Println("Postprocessed in:", time.Since(st))
-
-	fmt.Println("Detections:")
-	for i := range dets {
-		fmt.Println(dets[i])
 	}
 
 }
